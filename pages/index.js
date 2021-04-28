38d4a85f528bf8034fb6d54343ea2358e281700b
zainbintariq123 <zaintariq.alphasquad@gmail.com>
2021-04-28 14:37:28 +0500

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,56 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect, createRef, useState } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {TweenMax, Power3, gsap} from 'gsap';
+import { TweenMax, Power3, gsap } from 'gsap';
 // import  CSSRulePlugin  from 'gsap/CSSRulePlugin';
-import  { CSSRulePlugin }  from "gsap/dist/CSSRulePlugin";
+import { CSSRulePlugin } from "gsap/dist/CSSRulePlugin";
 
 // /if(typeof window !== 'undefined') {
-  gsap.registerPlugin(CSSRulePlugin);
+gsap.registerPlugin(CSSRulePlugin);
 // }
 
 export default function Home() {
+  const [refArray,setRefArray] = useState([]);
+  const buttons = [
+    {
+      data: 'Microwave'
+    },
+    {
+      data: 'freezer'
+    },
+    {
+      data: 'Cupboard'
+    },
+    {
+      data: 'Stave'
+    },
+    {
+      data: 'Fridge'
+    }
+  ];
+
+  let check = useRef(null);
+
+
+  
   let rule;
 
   if (typeof window !== 'undefined') {
     rule = CSSRulePlugin.getRule("span:after");
   }
 
-  let tl = gsap.timeline({defaults: {duration: 1}});
-  
-  let firstElement = useRef(null);
-  let secondElement = useRef(null);
-  let content = useRef(null);
-  let images = useRef(null);
-  let box = useRef(null);
-
-  
-  useEffect(()=>{
+  const changePlace = () =>{
+    // let value= refArray[i];
     TweenMax.to(
-      firstElement,
+      check,
       .8,
       {
-        opacity: 1,
-        y: -65,
-        ease: Power3.easeOut
+        y: 12,
+        delay:.3
       }
     )
-    TweenMax.to(
-      secondElement,
-      .8,
-      {
-        opacity:1,
-        opacity: 1,
-        y: -65,
-        ease: Power3.easeOut,
-        delay: .5
-      }
-    )
-
-    tl.from( content,.8,{  y: -50, ease: Power3.easeInOut, stagger: 0.6})
-    .to( rule, { duration :1.8, cssRule: { scaleY : 0 }},"-=2.2")
-    .from(box, .8, {  backgroundPosition: '300px 12px', opacity: 0},"-=1.5")
-    .from(images, .9, { duration: 1, y:30, opacity: 0, x:40})
-  })
-
-  const startAnimation = () =>{
-    tl.reversed() ? tl.play() : tl.reverse();
   }
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -65,57 +59,36 @@ export default function Home() {
       </Head>
 
       <main >
-        <div className="flex flex-row items-center justify-center">
-          <div
-           className="flex flex-col w-2/5">
-            <div>
+        <div className="bg-red-600 flex flex-col h-screen items-center justify-center">
+          <div className="flex flex-row items-center justify-center w-full">
+            <div className="w-2/6">
               <h2
-                ref={el=>{content = el}}
-                className="font-extrabold text-4xl w-3/6">
-                <span>
-                  Build your first app with gsap
-                </span>
-              </h2>
+                className="font-extrabold text-4xl ">
+                Don't store tomatoes in the as they will lose their taste
+                </h2>
             </div>
-            <div className="mt-6">
-              <p
-                ref={el=>{content = el}}
-                className="opacity-100 w-11/12">
-                In publishing and graphic design, Lorem ipsum is a placeholder text
-                commonly used to demonstrate the visual form of a document or a
-                typeface without relying on meaningful content. Lorem ipsum ma
-                be
-              </p>
-            </div>
-            <div className="mt-12">
-              <button
-                ref={el=>{content = el}}
-                className="bg-purple-600 border focus-within:outline-none font-bold px-9 py-3 rounded-3xl text-white"
-                onClick={startAnimation}  
-                >
-                  Start Working
-              </button>              
-            
+            <div className={`${styles._border}`}>
+              <span className= "border-b-2 px-10"></span>
             </div>
           </div>
-          <div
-            ref={el=>{box=el}}
-            className={`${styles.slider} w-auto`}>
-            <img
-              ref={el=>{images = el}}
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/swirl.png"></img>
+          <div className="flex flex-row">
+              {
+              buttons.map((item,id)=>( 
+                <div>
+                  <button
+                  
+                    ref={ref=> {check = ref }}
+                  
+                    className="bg-white border ml-2 mt-5 focus-within:outline-none px-6 py-2 rounded-2xl"
+                    onClick={changePlace}
+                  >
+                    {item.data}
+                  </button>
+                </div>
+              ))
+            }  
           </div>
         </div>
-          <div className="hidden" >
-            <img
-             ref={el=>{firstElement = el}}
-             className="w-1/2 h-1/2" src="/first.jpg" alt="next js starter" /> 
-            <p
-              ref={el=>{secondElement = el}} 
-              className="text-3xl my-5 ml-5"> 
-              Next js Starter
-            </p>
-          </div>
       </main>
     </div>
   )
